1b7e558cbf3f53f62522eacf44a1e2998031d899
Victor <victor.fernandes01@hotmail.com>
2020-06-26 09:50:41 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = [
+    { id: "KtcM-v7E1fY",
+      title: "GAULES CUMPRIU PROMESSA CORTE PEAKY BLINDERS! LUCAS1 CONTA HISTÓRIA COM KNG! CLUTCH DO KSCERATO!",
+      duration: "21:46",
+      price: "Play",
+      featured: true
+    },
+
+    { id: "l4iryWicXh0",
+      title: "FNX REAGINDO A CHAOS XITANDO CONTRA A MIBR!",
+      duration: "12:37",
+      price: "Play"
+    },
+
+    { id: "fVIC3lTMGls",
+      title: "KNG FALA A REAL SOBRE A CHAOS E DIFICULDADES! GAULES e FNX COMENTA SOBRE TRETA DO SUPOSTO CHEATER!!",
+      duration: "12:43",
+      price: "Play"
+    },
+
+    { id: "KDhJzup14TM",
+      title: "BILIONÁRIO DA ZUEIRA - Entrevista com Flavio Augusto da Silva",
+      duration: "1:21:52",
+      price: "Play",
+      featured: true
+    }
+];
